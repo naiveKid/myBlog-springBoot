a25d3c3f99ee09e6f47cf3193b23916a9495c76f
wolf master <1066394274@qq.com>
2020-05-30 22:00:27 +0800

    .

--- src/main/java/com/core/service/MoodService.java
+++ src/main/java/com/core/service/MoodService.java
@@ -13,10 +13,10 @@ import java.util.List;
 /**
  * 缓存机制说明：把sql查询的结果放到了redis中去, 然后第二次发起该条查询时从redis中去读取查询的结果，从而达到优化的效果.
  * 
- * @Cacheable(value="a")先查询redis是否存在相关结果，有则直接凑够redis中读取，否则从数据库获取数据并存放到redis中，以便下次直接从redis中读取。
- * 若不指定key的值，则会按照默认的规则生成；此处采用了自定义key。
- * @CachePut(value="a")先执行方法，最后再将该方法的查询结果放到redis中去，该方法始终会被spring调用.
- * @CacheEvict(value={"a","b",allEntries=true)的意思就是执行该方法后要清除redis中key名称为a,b的数据.
+ * @Cacheable 先查询redis是否存在相关结果，有则直接凑够redis中读取，否则从数据库获取数据并存放到redis中，以便下次直接从redis中读取。
+ * 若不指定key的值，则会按照默认的规则生成。 key 代表存放的键, value 为存放的命名空间
+ * @CachePut 先执行方法，最后再将该方法的查询结果放到redis中去，该方法始终会被spring调用. key 代表存放的键, value 为存放的命名空间
+ * @CacheEvict 的意思就是执行该方法后要清除redis中key名称为a,b的数据. key 代表存放的键, value 为存放的命名空间
  */
 @Service
 public class MoodService {
